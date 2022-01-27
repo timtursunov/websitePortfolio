da607ddd5827a364bba5da4725b425052ae3928f
timtursunov <timtursgram@gmail.com>
2022-01-27 00:06:48 -0500

    98% done

--- app.js
+++ app.js
@@ -20,12 +20,78 @@ document.addEventListener('scroll', () => {
         scrollPercentage.innerHTML = `socials`
         nav.style.opacity = 1;
         nav.style.zIndex = 3
+    }    else if (limit === 30) {
+        scrollPercentage.innerHTML = `tim turs`
     } else {
         scrollPercentage.innerHTML = `${pixels.toFixed()}px`
         nav.style.opacity = 0;
         nav.style.zIndex = -1
     }
-})
-// render
 
-const renderer = new THREE.GLRenderer()
+})
+const skillsBtn = document.querySelector('#skillsBtn');
+const portfolioBtn = document.querySelector('#portfolioBtn');
+let portfolioTexts = document.querySelectorAll('.portfolio__text')
+let skillsHighlighted = document.querySelectorAll('.portololio__skill')
+skillsBtn.addEventListener('click', ()=> {
+    portfolioBtn.style.borderColor = 'transparent'
+    portfolioBtn.style.color = 'black'
+
+    skillsBtn.style.borderColor = '#f42e0e'
+    skillsBtn.style.color = '#f42e0e'
+
+    skillsHighlighted.forEach(skill => {
+        skill.style.color ='#f42e0e'
+    })
+    portfolioTexts.forEach(text => {
+        text.style.color = '#a0a0a0'
+    })
+})
+portfolioBtn.addEventListener('click', ()=> {
+    skillsBtn.style.borderColor = 'transparent'
+    skillsBtn.style.color = 'black'
+
+    portfolioBtn.style.borderColor = '#f42e0e'
+    portfolioBtn.style.color = '#f42e0e'
+
+    skillsHighlighted.forEach(skill => {
+        skill.style.color ='black'
+    })
+    portfolioTexts.forEach(text => {
+        text.style.color = 'black'
+    })
+})
+let animatedTags = document.querySelectorAll("h1, .portfolio__text, .bio__text, .hero__text,  a, .portololio__skill, .hero__svg")
+animatedTags.forEach(tag => {
+    tag.style.opacity = 0
+})
+const FadeIn = () => {
+    console.log('fade in')
+    let delay = 0.15
+
+    animatedTags.forEach(tag => {
+        const tagsTop = tag.getBoundingClientRect().top
+        const tagsBottom = tag.getBoundingClientRect().bottom
+        // console.log(tagsTop, tagsBottom)
+        if(tagsTop < window.innerHeight && tagsBottom > 0
+            // if its in the view
+            ) {
+                tag.style.animation = `fadein 0.5s ${delay}s ease-in both`
+                delay = delay + 0.05
+            } else {
+                tag.style.opacity = 0
+                tag.style.animation = ''
+            }
+    })
+}
+FadeIn()
+document.addEventListener("scroll", FadeIn )
+window.addEventListener("resize", () => {
+    FadeIn()
+})
+// loc
+
+const locomotiveScroll = new LocomotiveScroll({
+     el: document.querySelectorAll('.scroll'),
+     smoth: true
+})
